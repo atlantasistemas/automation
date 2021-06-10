c181ef5af4a29efd299e5d46eced3c3cd6e46835
erli <erli-silva@hotmail.com>
2021-06-10 19:48:56 -0300

    Versão Incial

---
+++ Automations/AutExample.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Atl.Dal.Core;
+using Atl.Bll.Core;
+
+public class AutExample : AutomacaoBase
+{
+    public override void Run()
+    {
+        var sql = "SELECT codigo, razaosocial FROM empresa;";
+        new DbHelper(Session).ExecuteReader(sql, reader =>
+        {
+            var empresas = new List<string>();
+            while (reader.Read())
+            {
+                empresas.Add(reader.GetInt32(0) + " - " + reader["razaosocial"]);
+            }
+
+            var mensagem = String.Join(Environment.NewLine, empresas);
+            var assunto = "Mensagem enviada pelo robô";
+            var emailsDestino = new[] {"email_destino@hotmail.com"};
+            EnviaMensagemEmail(emailsDestino, assunto, mensagem);
+        });
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using Atl.Dal.Core;
+using Atl.Dto;
+
+namespace automation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Init();
+            
+            new AutExample().Start();
+        }
+
+        private static void Init()
+        {
+            Session.Connection = new ConnectionDataBase
+            {
+                Database = "",
+                Port = 0,
+                Server = "",
+                User = "",
+                PasswordText = "",
+            };
+            Session.Version = Assembly.GetExecutingAssembly().GetName().Version;
+
+            SessionLoader.LoadSession();
+        }
+    }
+}
